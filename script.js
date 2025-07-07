5953be9d35b97225bd6374c2e9e3ffb804150fcb
Ashwanth-Raj <ashwanthraj30@gmail.com>
2025-07-08 00:22:27 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+ScrollReveal().reveal('.hero-left', { origin: 'left', distance: '50px', duration: 1000, delay: 300 });
+ScrollReveal().reveal('.hero-right', { origin: 'right', distance: '50px', duration: 1000, delay: 500 });
+ScrollReveal().reveal('.navbar', { origin: 'top', distance: '20px', duration: 800, delay: 200 });
+ScrollReveal().reveal('.education-item', { origin: 'bottom', distance: '30px', duration: 800, interval: 200 });
+ScrollReveal().reveal('.internship-card', { origin: 'bottom', distance: '30px', duration: 800, interval: 150 });
+ScrollReveal().reveal('.certification-card', { origin: 'bottom', distance: '30px', duration: 800, interval: 150 });
+
+ScrollReveal().reveal('.about-img', {
+  origin: 'left',
+  distance: '50px',
+  duration: 1000,
+  delay: 200
+});
+ScrollReveal().reveal('.about-content', {
+  origin: 'right',
+  distance: '50px',
+  duration: 1000,
+  delay: 300
+});
+
+
+ScrollReveal().reveal('.contact-section', {
+  origin: 'bottom',
+  distance: '60px',
+  duration: 1000,
+  delay: 300,
+});
